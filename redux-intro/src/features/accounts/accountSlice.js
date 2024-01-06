70868ddc927159d8d4b21db589f2577185adc2cb
RPSingh0 <rpalsingh715@gmail.com>
2024-01-06 20:40:24 +0530

    before using redux-toolkit ⚛️

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -1,13 +1,14 @@
 const initialStateAccount = {
     balance: 0,
     loan: 0,
-    loanPurpose: ""
+    loanPurpose: "",
+    isLoading: false
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
     switch (action.type) {
         case "accounts/deposit":
-            return {...state, balance: state.balance + action.payload};
+            return {...state, balance: state.balance + action.payload, isLoading: false};
         case "accounts/withdraw":
             return {...state, balance: state.balance - action.payload};
         case "accounts/requestLoan":
@@ -23,13 +24,29 @@ export default function accountReducer(state = initialStateAccount, action) {
             }
         case "accounts/payLoan":
             return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan};
+        case "account/convertingCurrency":
+            return {...state, isLoading: true};
         default:
             return state;
     }
 }
 
-export function deposit(amount) {
-    return {type: "accounts/deposit", payload: amount};
+export function deposit(amount, currency) {
+
+    if (currency === "USD")
+        return {type: "accounts/deposit", payload: amount};
+
+    return async function (dispatch, getState) {
+
+        dispatch({type: "account/convertingCurrency"});
+
+        // API call
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`);
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        dispatch({type: "accounts/deposit", payload: converted});
+    }
 }
 
 export function withdraw(amount) {

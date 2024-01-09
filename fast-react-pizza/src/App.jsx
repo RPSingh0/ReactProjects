ad55fff96ff5f34427071312054c7df5314c7ece
RPSingh0 <rpalsingh715@gmail.com>
2024-01-09 23:52:40 +0530

    almost completed section 22 😊

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,8 +1,49 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order, {loader as orderLoader} from "./features/order/Order.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
+
+const router = createBrowserRouter([
+    {
+        // since this layout does not have a path, it is called a Layout-Route
+        element: <AppLayout/>,
+        errorElement: <Error/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/menu",
+                element: <Menu/>,
+                loader: menuLoader,
+                errorElement: <Error/>,
+            },
+            {
+                path: "/cart",
+                element: <Cart/>
+            },
+            {
+                path: "/order/new",
+                element: <CreateOrder/>
+            },
+            {
+                path: "/order/:orderId",
+                element: <Order/>,
+                loader: orderLoader,
+                errorElement: <Error/>,
+            }
+        ]
+    }
+]);
+
 function App() {
     return (
-        <div>
-            Hello Vite!
-        </div>
+        <RouterProvider router={router}/>
     );
 }
 

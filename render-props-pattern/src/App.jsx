400a7f6ff94bff444b4969ef388b23bdbbcb87a4
RPSingh0 <rpalsingh715@gmail.com>
2024-01-28 19:41:10 +0530

    Higher Order Components (HOC) patterns completed ⚡

--- render-props-pattern/src/App.jsx
+++ render-props-pattern/src/App.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {faker} from "@faker-js/faker";
 import "./styles.css";
+import withToggles from "./HOC.jsx";
 
 const products = Array.from({length: 20}, () => {
     return {
@@ -78,23 +79,6 @@ function List({title, items, render}) {
     );
 }
 
-export default function App() {
-    return (
-        <div>
-            <h1>Render Props Demo</h1>
-
-            <div className="col-2">
-                <List title="Products" items={products} render={(product) => (
-                    <ProductItem key={product.productName} product={product}/>
-                )}/>
-                <List title="Companies" items={companies} render={(company) => (
-                    <CompanyItem key={company.companyName} company={company} defaultVisibility={false}/>
-                )}/>
-            </div>
-        </div>
-    );
-}
-
 // LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
 function ProductList({title, items}) {
     return (
@@ -105,3 +89,28 @@ function ProductList({title, items}) {
         </ul>
     );
 }
+
+const ProductListWithToggles = withToggles(ProductList);
+
+export default function App() {
+    return (
+        <div>
+            <h1>Render Props Demo</h1>
+
+            {/*<div className="col-2">
+                <List title="Products" items={products} render={(product) => (
+                    <ProductItem key={product.productName} product={product}/>
+                )}/>
+                <List title="Companies" items={companies} render={(company) => (
+                    <CompanyItem key={company.companyName} company={company} defaultVisibility={false}/>
+                )}/>
+            </div>*/}
+            <div className={"col-2"}>
+                <ProductList title={"Products HOC"} items={products}/>
+                <ProductListWithToggles title={"Products HOC"} items={products}/>
+            </div>
+        </div>
+    );
+}
+
+
